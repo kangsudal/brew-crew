86ee0f2b097f6533ad524674f561390486744fc4
kangsudal <kangsudal@gmail.com>
2019-11-29 18:59:13 +0900

    #9 - Signing Out

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,26 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("home"),
-    );
+    return Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text("Brew Crew Home"),
+          backgroundColor: Colors.brown[400],
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text("logout"),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ));
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,17 +1,22 @@
 import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
-// import 'package:brew_crew/screens/home/home.dart';
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+/* 
+인증 상태에 따라 알맞은 screen widget을 반환해줌
+*/
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final user = Provider.of<User>(context);
-    print("user:$user");
 
     //return either Home or Authenticate widget
-    return Authenticate();
+    if (user == null) {//로그인이 안돼있을때
+      return Authenticate();
+    } else {//로그인이되어있을때
+      return Home();
+    }
   }
 }
