89d26f0e2b28fb58daca871c5a119d2edb9e6504
kangsudal <kangsudal@gmail.com>
2019-11-29 13:54:19 +0900

    #6 - Custom User Model

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -33,7 +33,7 @@ class _SignInState extends State<SignIn> {
             }
             else{
               print("signed in");
-              print(result);
+              print(result.uid);
             }
           },
         ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,21 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    //FirebaseUser 객체 데이터를 필요한것만 뽑아서 내 User 객체에 정리해서 담는다.
+    return user != null ? User(uid: user.uid) : null;
+  }
   // sign in anon
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     }catch(e) {
       print(e.toString());
       return null;
