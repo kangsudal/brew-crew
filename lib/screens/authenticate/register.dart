c7fa289100aeddb9364fe186736d862caf0a804e
kangsudal <kangsudal@gmail.com>
2019-12-05 14:19:38 +0900

    #14 - Text Input Decoration

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 /*
 Sign up 레이아웃 위젯.
@@ -39,78 +40,72 @@ class _RegisterState extends State<Register> {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white, width: 2.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.pink, width: 2.0),
-                  ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
                 ),
-                validator: (val) => val.isEmpty ? "Email 입력란이 비어있습니다!" : null,
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                validator: (val) =>
-                    val.length < 6 ? "Enter a password 6+ chars long" : null,
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RaisedButton(
-                color: Colors.brown,
-                child: Text(
-                  "Register",
-                  style: TextStyle(color: Colors.white),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Email"),
+                  validator: (val) => val.isEmpty ? "Email 입력란이 비어있습니다!" : null,
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    //access service function
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                        email, password);
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration:
+                      textInputDecoration.copyWith(hintText: "Password"),
+                  validator: (val) =>
+                      val.length < 6 ? "Enter a password 6+ chars long" : null,
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  color: Colors.brown,
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      //access service function
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
 
-                    if (result == null) {
-                      setState(() => error = "올바르게 email을 적었는지 확인해주세요!");
+                      if (result == null) {
+                        setState(() => error = "올바르게 email을 적었는지 확인해주세요!");
+                      }
+                      print(email);
+                      print(password);
                     }
-                    print(email);
-                    print(password);
-                  }
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14),
-              )
-            ],
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 /*
 sign in 레이아웃 위젯.
@@ -38,65 +39,70 @@ class _SignInState extends State<SignIn> {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                validator: (val) => val.isEmpty ? "Email 입력란이 비어있습니다!" : null,
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                validator: (val) =>
-                    val.length < 6 ? "Enter a password 6+ chars long" : null,
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RaisedButton(
-                child: Text("Sign in"),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    //access service function
-                    dynamic result = await _auth.signInWithEmailAndPassword(email,password);
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Email"),
+                  validator: (val) => val.isEmpty ? "Email 입력란이 비어있습니다!" : null,
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: textInputDecoration.copyWith(hintText: "Password"),
+                  validator: (val) =>
+                      val.length < 6 ? "Enter a password 6+ chars long" : null,
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("Sign in"),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      //access service function
+                      dynamic result =
+                          await _auth.signInWithEmailAndPassword(email, password);
 
-                    if (result == null) {
-                      setState(() {
-                        error = "could not sign in with those credentials.";
-                      });
-                    } else {
-                      print("signed in");
-                      print(result.uid);
+                      if (result == null) {
+                        setState(() {
+                          error = "could not sign in with those credentials.";
+                        });
+                      } else {
+                        print("signed in");
+                        print(result.uid);
+                      }
                     }
-                  }
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14),
-              )
-            ],
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                )
+              ],
+            ),
           ),
         ),
       ),
